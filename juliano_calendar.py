7e34b9bbacfea926e4d3cb057d9bf21426d24f0c
MRodriguezValdes <mrodriguezvaldes@cifpfbmoll.eu>
2022-11-28 10:03:14 +0100

    Error corregido

--- juliano_calendar.py
+++ juliano_calendar.py
@@ -1,10 +1,10 @@
-def bisiesto(anno):
+def bisiesto(anno:int)->bool:
         
-    return (anno%4==0 and anno%100!=0) or (anno%400==0)
+    return(anno%4==0 and anno%100!=0) or (anno%400==0)
     
 
 
-def num_of_day (month, anno):
+def num_of_day (month:int, anno:int)->int:
 
     if month  in [1,3,5,7,8,10,12]:
         return 31
@@ -18,12 +18,12 @@ def num_of_day (month, anno):
 
 
 
-def calculate_juliano_day(day,month,anno):
+def calculate_juliano_day(day:int,month:int,anno:int)->int:
     juliano_day=0
 
-    for i in range(month-1):
+    for i in range(1,month):
         juliano_day+= num_of_day(i,anno)
-
+    
     juliano_day += day
 
     return juliano_day
@@ -34,9 +34,8 @@ def calculate_juliano_day(day,month,anno):
 
 if __name__=="__main__":
     day = int(input("Give a day: "))
-    month = int(input("Give a month: "))
+    month =int(input("Give a month: "))
     anno = int(input("Give a anno: "))
 
     print(calculate_juliano_day(day,month,anno))
-
 
